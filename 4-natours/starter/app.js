48ca305e2f09dbdddd49f436661e974c52783275
Aditya Prasetyo <aditya.prasetyo@samb.co.id>
2022-10-24 20:31:45 +0700

    54. Responding to URL Parameters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -30,6 +30,31 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+app.get('/api/v1/tours/:id/:optional?', (req, res) => {
+    console.log(req.params);
+    // result ud in req.params.id is 'string', it is a trick to have a return as int by multiply it to 1
+    const id = req.params.id * 1;
+
+    const tour = tours.find((el) => el.id === id);
+
+    // id validation is exist ?
+    // if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: 'Fail!',
+            message: 'Invalid ID',
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        length: tours.length,
+        data: {
+            tour,
+        },
+    });
+});
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
