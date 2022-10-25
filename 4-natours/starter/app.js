82dd2fb910c3704491b7dd428d8de9ace0c472b7
Aditya Prasetyo <aditya.prasetyo@samb.co.id>
2022-10-25 20:50:10 +0700

    56. Handling DELETE Requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -97,6 +97,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'Fail!',
+            message: 'Invalid ID given',
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
