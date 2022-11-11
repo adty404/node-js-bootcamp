e64db0cd2d7b856695d100776011954a3168e09c
Aditya Prasetyo <aditya.prasetyo@samb.co.id>
2022-11-11 22:32:45 +0700

    95. Making the API Better: Filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -12,8 +12,24 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // BUILD QUERY
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
+        excludedFields.forEach((el) => delete queryObj[el]);
+
+        const query = await Tour.find(queryObj);
+
+        // const tours = await Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy');
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
