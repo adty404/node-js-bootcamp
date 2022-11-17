d0d4f4d31806e03ff6acb4ca3be169320f157c2d
Aditya Prasetyo <aditya.prasetyo@samb.co.id>
2022-11-17 22:39:37 +0700

    105. Document Middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 /*
     1) Require the mongoose module
@@ -15,6 +16,7 @@ const tourSchema = new mongoose.Schema(
             unique: true,
             trim: true,
         },
+        slug: String,
         duration: {
             type: Number,
             required: [true, 'A tour must have a duration'],
@@ -74,6 +76,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create() but not on .insertMany()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.pre('save', function (next) {
+//     console.log('Will save document...');
+//     next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//     console.log(doc);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
