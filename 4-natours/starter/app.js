69265d45b68a86b87145207509725ba7dad415a2
Aditya Prasetyo <adty404@gmail.com>
2022-12-29 10:52:57 +0700

    146. Preventing Parameter Pollution

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -52,6 +53,21 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    // if there are duplicate query parameters, only the last one will be used, except for the whitelisted ones
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price',
+        ],
+    })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
